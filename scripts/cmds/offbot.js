cdb0550088eeb2b8b489bffa402b8166ade2b417
chistytsu-stack <chistytsu@gmail.com>
2025-11-08 12:37:23 +0600

    Update offbot.js

--- scripts/cmds/offbot.js
+++ scripts/cmds/offbot.js
@@ -13,8 +13,8 @@ module.exports = {
   onStart: async function ({event, api}) {
     const permission = [ "100023789902793" ];
   if (!permission.includes(event.senderID)) {
-    api.sendMessage("✦━━━━━━━━━━━━━━━━━✦\n\n You don't have permission to use this command.\n\n✦━━━━━━━━━━━━━━━━━✦", event.threadID, event.messageID);
+    api.sendMessage("✦━━━━━━━━━━━━━━━━✦\n\n You don't have permission to use this command.\n\n✦━━━━━━━━━━━━━━━━✦", event.threadID, event.messageID);
     return;
   }
-    api.sendMessage("✦━━━━━━━━━━━━━━━━━✦\n\n Successfully Turned Off System ⊙ \n\n✦━━━━━━━━━━━━━━━━━✦",event.threadID, () =>process.exit(0))}
+    api.sendMessage("✦━━━━━━━━━━━━━━━━✦\n\n Successfully Turned Off System ⊙ \n\n✦━━━━━━━━━━━━━━━━✦",event.threadID, () =>process.exit(0))}
 };
